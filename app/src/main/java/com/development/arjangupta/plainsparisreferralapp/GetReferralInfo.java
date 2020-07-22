97cbae799ec549a8d142cb0385a8024d296b0774
Arjan Gupta <arjangupta95@gmail.com>
2020-07-22 00:38:25 -0500

    Add an empty second activity that intakes the person-to-be-referred's information

---
+++ app/src/main/java/com/development/arjangupta/plainsparisreferralapp/GetReferralInfo.java
@@ -0,0 +1,13 @@
+package com.development.arjangupta.plainsparisreferralapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GetReferralInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_referral_info);
+    }
+}
