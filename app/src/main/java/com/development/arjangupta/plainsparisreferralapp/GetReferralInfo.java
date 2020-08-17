6c5c5bfbdf9c448a9ab711a4c0635f30956fe9ad
Arjan Gupta <arjangupta95@gmail.com>
2020-08-16 21:49:57 -0500

    Send a POST request to server, show Toast message in case of failure to send SMS

--- app/src/main/java/com/development/arjangupta/plainsparisreferralapp/GetReferralInfo.java
+++ app/src/main/java/com/development/arjangupta/plainsparisreferralapp/GetReferralInfo.java
@@ -1,14 +1,28 @@
 package com.development.arjangupta.plainsparisreferralapp;
 
 import android.os.Bundle;
-import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class GetReferralInfo extends AppCompatActivity {
+
+    private String _ngrok_url             = "http://fafd18114419.ngrok.io/sms";
+    private String _referree_phone_number = "";
+    private OkHttpClient _http_client     = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +43,45 @@ public class GetReferralInfo extends AppCompatActivity {
                 Log.d(SUBMIT_BUTTON_TAG, "The given phone number is: " + phone);
                 Log.d(SUBMIT_BUTTON_TAG, "The given email is: " + email);
                 Log.d(SUBMIT_BUTTON_TAG, "The given message is: " + message);
+
+                _referree_phone_number = phone;
+
+                try {
+                    // Send the POST message
+                    sendPOSTRequestToPlainsParis(_ngrok_url, new  Callback(){
+
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+
+                            Log.d(SUBMIT_BUTTON_TAG, "Response: " + response.message());
+
+                            if (500 == response.code()) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(getApplicationContext(), "SMS Failure", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                                return;
+                            }
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(),"SMS Sent to Plains Paris!",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    });
+                } catch (IOException exception) {
+                    exception.printStackTrace();
+                }
             }
 
             private static final String SUBMIT_BUTTON_TAG = "Submit Button";
@@ -38,5 +91,18 @@ public class GetReferralInfo extends AppCompatActivity {
                 return input_layout.getEditText().getText().toString();
             }
         });
+    }
+
+    void sendPOSTRequestToPlainsParis(String url, Callback callback) throws IOException{
+        RequestBody formBody = new FormBody.Builder()
+                .add("To", "+18166166041")
+                .add("Body", "Hello, Plains Paris. Somebody was referred to you.")
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+        Call response = _http_client.newCall(request);
+        response.enqueue(callback);
     }
 }
